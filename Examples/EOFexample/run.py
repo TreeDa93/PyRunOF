dc0f87a05fc734737382dd1bf13b9a6cae21ba77
Ivan Smolyanov <i.a.smolyanov@gmail.com>
2021-04-22 17:32:52 +0200

    I add a number of examples

---
+++ Examples/EOFexample/run.py
@@ -0,0 +1,32 @@
+import os, sys
+libpath = '/home/ivan/mySolvers/RunnerForCases/'
+sys.path.append(libpath)
+
+from Modules.Manipulations import Manipulations
+from Modules.RunApplication import Runner
+from Modules.Meshes import Mesh
+from Modules.setSystem import SetSystem
+from Modules.InitialValue import IntiailValue
+from Modules.setConstant import SetConstantParam
+
+
+basePath = '2D_TMFHartmann'
+
+
+if __name__ == "__main__":
+
+    mc = Manipulations(basePath=basePath)
+    mc.generatorNewName('solved', baseNewName=basePath)
+    newName = mc.getName('newName')
+    mc.createNewPath(dirmame=os.getcwd(), newCaseName=newName)
+    newPath = mc.getPath('newPath')
+    mc.dublicateCase(basePath=basePath, newPath=newPath, mode='rewrite')
+
+
+    rc = Runner()
+    rc.setCores(numCoreOF=4, numCoreElmer=4)
+    rc.setPathCase(newPath)
+    rc.setNameSolver(solverName='mhdVxBPhasePimpleFoam')
+    rc.setModeRunner(mode='EOF')
+    rc.setPyFoamSettings(pyFoam=False)
+    rc.runCase(decompose=True)
