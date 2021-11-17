86e65f411dabc59d849ba0f9c8fd56f94d02ff4c
TreeD <i.a.smolyanov@gmail.com>
2021-11-17 16:26:36 +0500

    add new class in additionaFunctions

--- Modules/AddtionalFunctions.py
+++ Modules/AddtionalFunctions.py
@@ -1,4 +1,6 @@
-import os, shutil
+import os
+import shutil
+
 
 def changeVariablesFunV2(distVar, sourVar, nameFile=''):
     """Function to find and replace required text part at given file
@@ -6,15 +8,15 @@ def changeVariablesFunV2(distVar, sourVar, nameFile=''):
     sourVar depicts replacing variables
     nameFile is the name of file where the procedure will be done"""
 
-
     if os.path.isfile(nameFile):
-        with open (nameFile, 'r') as f:
+        with open(nameFile, 'r') as f:
             oldData = f.read()
         newData = oldData.replace(str(distVar), str(sourVar))
         with open(nameFile, 'w') as f:
             f.write(newData)
     else:
         print(f'Warning: The file {nameFile} is not exist!')
+
 
 def changeVariablesFun(distVar, sourVar, nameFile=' '):
     """The fucntion is devoted to change  gotten text part
@@ -37,8 +39,8 @@ def copyfun(root_src_dir, root_dst_dir):
                 os.remove(dst_file)
             shutil.copy(src_file, dst_dir)
 
-def copyFieles(root_src_dir, root_dst_dir):
 
+def copyFieles(root_src_dir, root_dst_dir):
     for file_ in os.listdir(root_src_dir):
         src_file = os.path.join(root_src_dir, file_)
         dst_file = os.path.join(root_dst_dir, file_)
@@ -46,7 +48,34 @@ def copyFieles(root_src_dir, root_dst_dir):
             os.remove(dst_file)
         shutil.copy(src_file, dst_file)
 
+
 def copyFiele(root_src_dir, root_dst_dir, nameFilesOld='', nameFileNew=''):
-        src_file = os.path.join(root_src_dir, nameFilesOld)
-        dst_file = os.path.join(root_dst_dir, nameFileNew)
-        shutil.copy2(src_file, dst_file)
+    src_file = os.path.join(root_src_dir, nameFilesOld)
+    dst_file = os.path.join(root_dst_dir, nameFileNew)
+    shutil.copy2(src_file, dst_file)
+
+
+class AddtionalFun:
+    """ The class supports
+
+    """
+
+    def __init__(self):
+        pass
+
+    def change_text(self, dist_var, sour_var, name_file=''):
+        """Function to find and replace required text part at given file
+        distVar is the depicts finding variables
+        sourVar depicts replacing variables
+        nameFile is the name of file where the procedure will be done
+
+        """
+
+        if os.path.isfile(dist_var):
+            with open(sour_var, 'r') as f:
+                oldData = f.read()
+            newData = oldData.replace(str(dist_var), str(sour_var))
+            with open(name_file, 'w') as f:
+                f.write(newData)
+        else:
+            print(f'Warning: The file {name_file} is not exist!')
